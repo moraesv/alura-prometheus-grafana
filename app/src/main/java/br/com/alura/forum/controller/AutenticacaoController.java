792dbf36bf1ea5e92a4ad5b6c6b1e65274ff202f
vinicius.moraes <vinicius.carvalho@luizalabs.com>
2025-02-25 20:13:42 -0300

    proxy e client

--- app/src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
+++ app/src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
@@ -1,7 +1,6 @@
 package br.com.alura.forum.controller;
 
 import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import br.com.alura.forum.config.security.TokenService;
 import br.com.alura.forum.controller.dto.TokenDto;
 import br.com.alura.forum.controller.form.LoginForm;
@@ -29,15 +27,15 @@ public class AutenticacaoController {
 	Counter authUserErrors;
 
 	public AutenticacaoController(MeterRegistry registry) {
-		authUserSuccess = Counter.builder("auth_user_success")
-			.description("usuarios autenticados")
-			.register(registry);
-			
-		authUserErrors = Counter.builder("auth_user_error")
-			.description("erros de login")
-			.register(registry);
-	}
-	
+    	authUserSuccess = Counter.builder("auth_user_success")
+            .description("usuarios autenticados")
+            .register(registry);
+    	
+    	authUserErrors = Counter.builder("auth_user_error")
+                .description("erros de login")
+                .register(registry);
+    }
+    
 	@Autowired
 	private AuthenticationManager authManager;
 	
@@ -53,11 +51,10 @@ public class AutenticacaoController {
 			String token = tokenService.gerarToken(authentication);
 			authUserSuccess.increment();
 			return ResponseEntity.ok(new TokenDto(token, "Bearer"));
+			
 		} catch (AuthenticationException e) {
 			authUserErrors.increment();
 			return ResponseEntity.badRequest().build();
-		}
-
-		
+		}	
 	}
 }
